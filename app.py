f9e5ed23ae6b59d45fbacfe0063b78c992074925
zzttyy413841467 <zzttyy413841467@gmail.com>
2018-08-08 18:58:08 +0800

    app.py version 0.0.1

--- app.py
+++
@@ -1,2 +0,0 @@
-print('hollow world')
-print('next time')
--- www/app.py
+++ www/app.py
@@ -1,1 +1,18 @@
-print('hollow world')
+import logging;logging.basicConfig(level=logging.INFO)
+import asyncio,os,json,time
+from datetime import datetime
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>Awesome</h1')
+
+@asyncio.coroutine
+def init(loop):
+    app=web.Application(loop=loop)
+    app.router.add_route('GET','/',index)
+    srv=yield from loop.creat_server(app.make_handler(),'127.0.0.1',9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+loop=asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
