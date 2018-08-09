ffc5dac9d719d8696ce468c6791a87ea47b4e499
zzttyy413841467 <zzttyy413841467@gmail.com>
2018-08-09 10:34:20 +0800

    app.py version 0.0.2 适配aiohttp3

--- www/app.py
+++ www/app.py
@@ -7,14 +7,18 @@ def index(request):
     return web.Response(body=b'<h1>Awesome</h1',content_type="text/html")
 
 
-async def init(event_loop):
-    app=web.Application(loop=event_loop)
-    app.router.add_route('GET','/',index)
+async def init():
+    app=web.Application()
+    app.add_routes([web.get('/',index)])
     app_runner=web.AppRunner(app)
-    srv = await event_loop.create_server(app_runner.app.make_handler(), '127.0.0.1', 9000)
+    #srv = await event_loop.create_server(app_runner.app.make_handler(), '127.0.0.1', 9000)
+    await app_runner.setup()
+    site = web.TCPSite(app_runner,'127.0.0.1',9000)
+    await site.start()
     logging.info('server started at http://127.0.0.1:9000...')
-    return srv
+    #return srv
 
 loop=asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
+loop.run_until_complete(init())
 loop.run_forever()
+
