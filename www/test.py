a861924a890789a68c6613ce4d29a8af13df496c
zzttyy413841467 <zzttyy413841467@gmail.com>
2018-08-10 09:58:11 +0800

    test read and write

--- www/test.py
+++ www/test.py
@@ -4,15 +4,17 @@ from models import User,Blog,Comment
 
 async def test(loop):
     await orm.create_pool(user='www',password='www',db='awesome',loop=loop)
-    u=User(name='Test4',email='test4@email.com',passwd='12564687',image='about:blank')
+    u=User(name='Test5',email='test5@email.com',passwd='12564687',image='about:blank')
     await u.save()
 
 async def find(loop):
     await orm.create_pool(user='www',password='www',db='awesome',loop=loop)
     rs = await User.findAll()
-    print('查找测试： %s/n' % rs)
+    for i in rs:
+        print('查找测试： %s' % i)
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(asyncio.wait([test(loop),find(loop)]))
 loop.close()
-
+if loop.is_closed():
+    exit(0)
